b35bcd7aa8f8486cb354dfc715d78ea42ed88d18
Delta935142 <s303446@gmail.com>
2021-06-25 17:09:52 +0800

    config

---
+++ config/mitake.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /**
+     * 三竹簡訊使用者帳號
+     */
+    'username' => env('MITAKE_USERNAME', ''),
+
+    /**
+     * 三竹簡訊使用者密碼
+     */
+    'password' => env('MITAKE_PASSWORD', ''),
+];
